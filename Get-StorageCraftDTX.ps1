f2ac59cbaea813494467403d55c2a9b8a6c4ad61
bradsmr <bradsmr@gmail.com>
2021-10-01 17:05:56 -0700

    Initial commit

---
+++ Get-StorageCraftDTX.ps1
@@ -0,0 +1,10 @@
+﻿$url = 'https://downloads.storagecraft.com/SP_Files/dtx-2.3.0-0_windows-x86_64_2021-08-05.zip'
+$DownloadZipFile = "C:\dtx\zip\" + $(Split-Path -Path $Url -Leaf)
+$ExtractPath = "C:\dtx\"
+New-Item -ItemType Directory -Force -Path C:\dtx\zip
+Invoke-WebRequest -Uri $Url -OutFile $DownloadZipFile
+$ExtractShell = New-Object -ComObject Shell.Application 
+$ExtractFiles = $ExtractShell.Namespace($DownloadZipFile).Items() 
+$ExtractShell.NameSpace($ExtractPath).CopyHere($ExtractFiles) 
+Start-Process $ExtractPath
+Start-Process -NoNewWindow -FilePath "C:\dtx\dtx.exe"
